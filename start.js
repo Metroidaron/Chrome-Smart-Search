e9852dc64a4218e247f4921189b8610e85932bfe
Joshua Carver <mar21josh@gmail.com>
2017-08-03 22:50:07 -0400

    Created Script for server start, adding static routes for Local CDN

---
+++ start.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+//Prepare Server
+app.use('/', express.static(__dirname + '/public')); //Redirect Root folder
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+app.listen(8080);
