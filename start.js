857cbee3a99b8fe757ddb2a55726f3d69a1f90c2
Joshua Carver <Mar21josh@gmail.com>
2017-09-04 20:28:26 -0400

    Working On Settings page, Added Font Awesome Support

--- start.js
+++ start.js
@@ -6,5 +6,6 @@ app.use('/', express.static(__dirname + '/public')); //Redirect Root folder
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts')); // redirect CSS bootstrap
+app.use('/font-awesome', express.static(__dirname + '/node_modules/font-awesome')) // Redirects to Font-Awesome
 
 app.listen(8080);
