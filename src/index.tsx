3bb4402097af054535f7be3d1f18898cb9a70b9f
Joshua Carver <joshua.carver@ahss.org>
2017-12-28 13:38:18 -0500

    Added Chrome Smart Search parent module and set it to render in place of the default 'app'

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import ChromeSmartSearch from './ChromeSmartSearch.module';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root') as HTMLElement
+// );
+
+ReactDOM.render( <ChromeSmartSearch />, document.getElementById('root') as Element);
+
 registerServiceWorker();
