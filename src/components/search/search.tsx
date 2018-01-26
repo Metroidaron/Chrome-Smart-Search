edf052998c8f00c465e60c47aa14b3086e778656
Joshua Carver <mar21josh@gmail.com>
2018-01-26 16:52:47 -0500

    Configured and styled Search Box

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -11,28 +11,82 @@ export interface iProps{
 
 export interface iState{
     settings : iSettings;
+    time : string;
 }
 
 export default class Search extends React.Component<iProps, iState>{
 
     sS : settingsStorage; 
+    interval : any;
 
     constructor(props: iProps){
         super(props);
         this.state = {
-            settings : {}
+            settings : {},
+            time : new Date(Date.now()).toLocaleTimeString()
         }
         this.sS = new settingsStorage();
+        this.getTime = this.getTime.bind(this);
     }
 
     componentDidMount(){
         this.setState({settings : this.sS.getSettingsObject()})
+        this.interval = window.setInterval(()=>{this.getTime()}, 10);
+    }
+
+    getTimeFrame(): string{
+        let d = new Date();
+        let timeFrame = "";
+        switch(d.getHours()){
+            case 0 : timeFrame = "Good Morning"; break;
+            case 1 : timeFrame = "Good Morning"; break;
+            case 2 : timeFrame = "Good Morning"; break;
+            case 4 : timeFrame = "Good Morning"; break;
+            case 3 : timeFrame = "Good Morning"; break;
+            case 5 : timeFrame = "Good Morning"; break;
+            case 6 : timeFrame = "Good Morning"; break;
+            case 7 : timeFrame = "Good Morning"; break;
+            case 8 : timeFrame = "Good Morning"; break;
+            case 9 : timeFrame = "Good Morning"; break;
+            case 10 : timeFrame = "Good Morning"; break;
+            case 11 : timeFrame = "Good Morning"; break;
+            case 12 : timeFrame = "Good Afternoon"; break;
+            case 13 : timeFrame = "Good Afternoon"; break;
+            case 14 : timeFrame = "Good Afternoon"; break;
+            case 15 : timeFrame = "Good Afternoon"; break;
+            case 16 : timeFrame = "Good Afternoon"; break;
+            case 17 : timeFrame = "Good Afternoon"; break;
+            case 18 : timeFrame = "Good Evening"; break;
+            case 19 : timeFrame = "Good Evening"; break;
+            case 20 : timeFrame = "Good Evening"; break;
+            case 21 : timeFrame = "Good Evening"; break;
+            case 22 : timeFrame = "Good Evening"; break;
+            case 23 : timeFrame = "Good Evening"; break;
+          }
+        return timeFrame;
+    }
+
+    getTime(){
+        let d = new Date(Date.now());
+        this.setState({time: d.toLocaleTimeString()});
+        this.forceUpdate();
+    }
+
+    setBackground(){
+        let elem = document.getElementById("five");
+        let number = this.props.settings.getSettingsObject().wallpaperTint;
+        (elem != null || elem != undefined) ? elem.style.backgroundColor = 'rgba(0,0,0,' + number + ')' : null;
     }
 
     render(){
         return(
             <div id="searchLayer">
-                <div id="five">Welcome {this.props.settings.getSettingsObject().firstName}</div>
+                <div id="five">
+                    {this.getTimeFrame()} {this.props.settings.getSettingsObject().firstName}, It's currently {this.state.time}
+                    <br />
+                    <div id="query"><input type="text" placeholder="Ask Something..." /></div>
+                </div>
+                {this.setBackground()}
             </div>  
         )
     }
